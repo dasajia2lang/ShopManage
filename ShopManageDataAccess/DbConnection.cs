e5e4caf907282afb5917fa252a1b9763f7c4fd62
Unknown <dasajia2lang@163.com>
2017-10-09 23:26:13 +0800

    add mysql connection and add get orderlist function

---
+++ ShopManageDataAccess/DbConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopManageDataAccess
+{
+    public class DbConnection
+    {
+        public static string ShopManageConnection= "server=127.0.0.1;database=shopmanagedb;uid=root@localhost;pwd=lid26298;charset='gbk'";
+    }
+}
--- ShopManageDataAccess/OrderDataAccess.cs
+++ ShopManageDataAccess/OrderDataAccess.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using ShopManageContact;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
 
 namespace ShopManageDataAccess
 {
     public class OrderDataAccess
     {
+        /// <summary>
+        /// 获取订单列表
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Order>> GetOrderList()
+        {
+            var list = new List<Order>();
+            using (var context = new MySqlConnection(DbConnection.ShopManageConnection))
+            {
+                list=await context.ExecuteScalarAsync<List<Order>>("select * from Order");
+            }
+            return list;
+        }
     }
 }
